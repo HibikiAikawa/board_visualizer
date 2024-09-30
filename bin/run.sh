4ace0e4479ca61efbdd6d8c893d63352bba0f9e7
HibikiAikawa <hibikingxfin@gmail.com>
2024-09-30 13:35:04 +0000

    add python_script

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT_DIR=$(dirname $(dirname $(realpath $0)))
 
-docker build ${ROOT_DIR}/docker -t board_visualizer
+docker build -f ./docker/Dockerfile . -t board_visualizer
 docker run -it -v ${ROOT_DIR}:/work -w /work board_visualizer
